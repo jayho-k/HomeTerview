c080244df84cdaaeb0ba8e1e75c16cf9086f9ac6
jayho <sox3579@gmail.com>
2023-01-19 01:08:51 +0900

    [BE] feat : create directories

---
+++ backend/src/test/java/com/jayho/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayho.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
